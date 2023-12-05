1c74b1c661b2f66b65e29d3826ffc5cbe2d254da
Margitoh <101721765+Margitoh@users.noreply.github.com>
2023-12-05 05:23:41 +0100

    progress on ItemCompare

--- src/components/ItemCompare.jsx
+++ src/components/ItemCompare.jsx
@@ -1,9 +1,88 @@
-function ItemCompare() {
+import { useState } from "react";
+import Tesseract from "tesseract.js";
+
+const ItemCompare = () => {
+  const [image, setImage] = useState(null);
+  const [processing, setProcessing] = useState(false);
+  const [ocrResult, setOcrResult] = useState(null);
+
+  const processImage = async (image) => {
+    setProcessing(true);
+    try {
+      const { data } = await Tesseract.recognize(image, "eng");
+
+      const lines = data.lines;
+      console.log("Lines:", lines);
+
+      const excludedPatterns = [
+        "Item Power",
+        "Resistance to All Elements",
+        "Critical Strikes that Overpower deal",
+        "Be DS of BRE Ol 0 Te)",
+      ];
+
+      const relevantLines = lines.filter((line, index) => {
+        const currentLineText = line.text;
+        const nextLineText =
+          index < lines.length - 1 ? lines[index + 1].text : "";
+
+        if (
+          excludedPatterns.some((pattern) => currentLineText.includes(pattern))
+        ) {
+          return false;
+        }
+
+        return (
+          currentLineText.includes("©") ||
+          nextLineText.includes("]") ||
+          nextLineText.includes("%")
+        );
+      });
+
+      setOcrResult(relevantLines);
+    } catch (error) {
+      console.error("Error processing image:", error);
+    } finally {
+      console.log("OCR process completed.");
+      setProcessing(false);
+    }
+  };
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const imageData = event.target.result;
+      setImage(imageData);
+      processImage(imageData);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
   return (
     <div>
-      <h1>Hello World test</h1>
+      <input type="file" onChange={(e) => handleImageUpload(e)} />
+      {image && (
+        <div>
+          <img src={image} alt="Uploaded Item" style={{ height: "500px" }} />
+          {processing ? (
+            <div>Processing...</div>
+          ) : (
+            <div>
+              <p>OCR Result:</p>
+              {ocrResult && ocrResult.length > 0 ? (
+                ocrResult.map((line, index) => <p key={index}>{line.text}</p>)
+              ) : (
+                <p>No relevant OCR result available</p>
+              )}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default ItemCompare;
---
+++ src/globals/faIcons.jsx
@@ -0,0 +1,7 @@
+import { faCamera } from "@fortawesome/free-solid-svg-icons/faCamera";
+import { FontAwesomeIcon as BaseFontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+
+export const Camera = faCamera;
+
+export const IconSize = 30;
+export const FontAwesomeIcon = BaseFontAwesomeIcon;
